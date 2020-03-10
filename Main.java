69fbfe330f9d9f886672a61a982f92634ec0ff65
trevor-calderwood <59665331+trevor-calderwood@users.noreply.github.com>
2020-03-09 17:36:21 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        MatrixRow myRow = new MatrixRow();
-        //ValueNode(Row Column Value)
-        ValueNode v1 = new Valuenode(1, 1, 23);
-        ValueNode v2 = new Valuenode(1, 3, 7);
-        ValueNode v3 = new Valuenode(1, 2, 88);
-
-        myRow.insert(v1);
-        myRow.insert(v2);
-        myRow.insert(v3);
-
-        int returnValue = myRow.
-
-        System.out.println("")
-
-
-
+        Homework2 program = new Homework2();
+        program.run();
     }
 }
