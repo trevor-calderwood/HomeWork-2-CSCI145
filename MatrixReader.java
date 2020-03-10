688296c2da6a5e0ec9e5a2a90b0e329194d1664f
Grant Sheridan <59630306+grant-sheridan@users.noreply.github.com>
2020-03-09 19:05:47 -0700

    Add files via upload

--- MatrixReader.java
+++ MatrixReader.java
@@ -3,13 +3,48 @@ import java.util.Scanner;
 public class MatrixReader {
 
     public SparseMatrix read(String file){
-        Scanner scan = new Scaner(new File(file));
+        Scanner scan = new Scanner(new File(file));
         int rows = scan.nextInt();
         int columns = scan.nextInt();
         String valueTemp;
         String tempLine;
-
+        
+        //could do a switch case method like
+        // switch(scan), case: matrixA <instert for loop> 
+        //  then do the same thing for case: matrixB
+        switch(scan){
+            case matrixA:
+                for(int i = 0; i <= rows; i++){
+                    tempLine = scan.nextLine();
+                    for(int j = 0; j <= tempLine.length(); j++){
+                        if(tempLine.charAt(j) == " "){
+                            valueTemp = tempLine.split(" ");
+                        }
+                     if(tempLine.charAt(j) == ","){
+                        valueTemp = tempLine.split(",");
+                        }
+                    }
+                }
+                return null;
+            
+            case matrixB:
+            for(int i = 0; i <= rows; i++){
+                tempLine = scan.nextLine();
+                for(int j = 0; j <= tempLine.length(); j++){
+                    if(tempLine.charAt(j) == " "){
+                        valueTemp = tempLine.split(" ");
+                    }
+                 if(tempLine.charAt(j) == ","){
+                    valueTemp = tempLine.split(",");
+                    }
+                }
+            }
+            return null;
+        }
+        /* Honestly, im stumped, I know there is a way we can parse through that for loop below
+         */
     }
+}
 
 
 
